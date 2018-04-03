eda7f0fc55e0bd13019d8290125a8d658274e54e
sophiamarinelli <31392946+sophiamarinelli@users.noreply.github.com>
2018-04-03 17:25:17 -0400

    about and contact addition

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-console.log("1")
+// console.log("1")
 
-document.addEventListener("click", function(){
-	console.log("1")
-	document.body.style.backgroundColor = "black"
-})
+// document.addEventListener("click", function(){
+// 	console.log("1")
+// 	document.body.style.backgroundColor = "black"
+// })
+
+$( function() {
+    $( ".viewer" ).draggable({containment:"document"});
+    $( ".viewer" ).resizable({handles: "n,s"});
+    $( ".bigtext" ).draggable({containment:"document"});
+    $( ".nav" ).draggable({containment:"document"});
+  } );
