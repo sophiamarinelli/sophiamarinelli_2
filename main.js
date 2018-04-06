5cf7a8e5b64d0a557a59e447f6279a6302264ccc
sophiamarinelli <31392946+sophiamarinelli@users.noreply.github.com>
2018-04-06 14:39:48 -0400

    add sxsw

--- main.js
+++ main.js
@@ -6,13 +6,57 @@
 // })
 
 $( function() {
-    $( ".viewer" ).draggable({containment:"document"}).resizable({aspectRatio: 4 / 3});
-    $( ".bigtext" ).draggable({containment:"document"});
+    $( ".viewer" ).draggable({
+    	// containment:"window",
+    });
+    $( ".bigtext" ).draggable({});
     $( ".nav" ).draggable({containment:"document"});
     $( ".about" ).draggable({containment:"document"}).resizable({});
   } );
 
-$(".viewer").click(function(){
-	console.log("1")
-    $(".viewer").css("zIndex", 9999);
- });
+
+// $(".magnolia, .recipes, .bowie").click(function(){
+//     //assign all divs `z-index` = 90 including clicked one
+//     $(".magnolia, .recipes, .bowie").css("z-index","90");
+//     $(".bowie").
+
+//     //assign `z-index` = 100 to clicked one
+//     $(this).css("z-index","100")
+// });
+
+// document.getElementById("bowie").onmouseover = function() {
+// 	document.getElementById("descriptionbox").style.display = "block";
+// 	console.log("1")
+// }
+
+// document.getElementById("bowie").onmouseout = function() {
+// 	document.getElementById("descriptionbox").style.display = "none";
+// 	console.log("1")
+// }
+
+$(".bowie").click(function(){
+    //assign all divs `z-index` = 90 including clicked one
+    $(".magnolia, .recipes, .bowie").css("z-index","90");
+    $("#descriptionbowie").css("display","inline-block")
+
+    //assign `z-index` = 100 to clicked one
+    $(this).css("z-index","100")
+});
+
+$(".magnolia").click(function(){
+    //assign all divs `z-index` = 90 including clicked one
+    $(".magnolia, .recipes, .bowie").css("z-index","90");
+    $("#descriptionmagnolia").css("display","inline-block")
+
+    //assign `z-index` = 100 to clicked one
+    $(this).css("z-index","100")
+});
+
+$(".recipes").click(function(){
+    //assign all divs `z-index` = 90 including clicked one
+    $(".magnolia, .recipes, .bowie").css("z-index","90");
+    $("#descriptionrecipes").css("display","inline-block")
+
+    //assign `z-index` = 100 to clicked one
+    $(this).css("z-index","100")
+});
