857f077a4ae27c7841448ca6e098566c0c314824
sophiamarinelli <31392946+sophiamarinelli@users.noreply.github.com>
2019-01-23 21:37:53 +0100

    simplified

--- changing.js
+++ changing.js
@@ -1,5 +1,5 @@
 function loadingBM(){
-  setTimeout('$("#besideMouse").html("IM CHANGING.")', 0);
+  setTimeout('$("#besideMouse").html("IM CHANGING. And you should believe that I will eventually return. (Eventually.)")', 0);
 }
 var loadingBMint;
 function startLoadingBM(){
@@ -33,28 +33,28 @@ $(document).ready(function(){
    //        });
    //      });
 
-   function generate() {
+//    function generate() {
 
-  var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+//   var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
   
-  function populate(a) {
-    for ( var i = 0; i < 6; i++ ) {
-      var x = Math.round( Math.random() * 14 );
-      var y = hexValues[x];
-      a += y;
-    }
-    return a;
-  }
+//   function populate(a) {
+//     for ( var i = 0; i < 6; i++ ) {
+//       var x = Math.round( Math.random() * 14 );
+//       var y = hexValues[x];
+//       a += y;
+//     }
+//     return a;
+//   }
   
-  var newColor1 = populate('#');
-  var newColor2 = populate('#');
-  var angle = Math.round( Math.random() * 360 );
+//   var newColor1 = populate('#');
+//   var newColor2 = populate('#');
+//   var angle = Math.round( Math.random() * 360 );
   
-  var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
+//   var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
   
-  document.body.style.background = gradient;
+//   document.body.style.background = gradient;
   
-}
+// }
 
-document.onclick = generate();
+// document.onclick = generate();
 });
